d7d65cb1e34e066fc0e48ef762f48e63e8ebe322
Howl <nhuthung0511@gmail.com>
2025-01-19 22:51:30 -0500

    updated the currency name of Lebanon

--- data/lebanon.js
+++ data/lebanon.js
@@ -3,13 +3,13 @@ let index = 1;
 const images = {
     [`${index}`]: [
         {
-            src: "../images/collection/lebanon/2011-2012/1000-pound-f.jpg",
-            alt: "Front of the 1000 Pound Bill",
+            src: "../images/collection/lebanon/2011-2012/1000-lira-f.jpg",
+            alt: "Front of the 1000 Lira Bill",
             description: "Obverse"
         },
         {
-            src: "../images/collection/lebanon/2011-2012/1000-pound-b.jpg",
-            alt: "Back of the 1000 Pound Bill",
+            src: "../images/collection/lebanon/2011-2012/1000-lira-b.jpg",
+            alt: "Back of the 1000 Lira Bill",
             description: "Reverse"
         }
     ]
@@ -18,7 +18,7 @@ createSlideshow("slide" + index, images[index]);
 new Slideshow("slideshow" + index);
 generateSlideShowInfo(
     "info" + index,
-    "Lebanese 1,000 Pounds",
+    "Lebanese 1,000 Lira",
     "",
     "Circulating Standard Banknote",
     "115 $\\times$ 60 mm",
