3ea7a910ffefa057a6cd521bb1cb98219a630c77
Howl <nhuthung0511@gmail.com>
2025-02-19 13:58:27 -0500

    updated the missing data

--- data/scotland.js
+++ data/scotland.js
@@ -3,17 +3,41 @@ let index = 1;
 const images = {
     [`${index}`]: [
         {
-            src: "../images/collection/scotland/clydesdale-bank/2016/5-pound-f.jpg",
-            alt: "Front of the 5 Pound Bill",
+            src: "../images/collection/scotland/royal-bank/1987-2001/1-pound-f.jpg",
+            alt: "Front of the 1 Pound Bill",
             description: "Obverse"
         },
         {
-            src: "../images/collection/scotland/clydesdale-bank/2016/5-pound-b.jpg",
-            alt: "Back of the 5 Pound Bill",
+            src: "../images/collection/scotland/royal-bank/1987-2001/1-pound-f.jpg",
+            alt: "Back of the 1 Pound Bill",
             description: "Reverse"
         }
     ]
 };
+createSlideshow("slide" + index, images[index]);
+new Slideshow("slideshow" + index);
+generateSlideShowInfo(
+    "info" + index,
+    "Scottish 1 Pound - Royal Bank",
+    "1992",
+    "Circulating Standard Banknote",
+    "128 $\\times$ 65 mm",
+    "Ilay Campbell (1734-1823)"
+);
+
+index = index + 1; //index = 2
+images[`${index}`] = [
+    {
+        src: "../images/collection/scotland/clydesdale-bank/2016/5-pound-f.jpg",
+        alt: "Front of the 5 Pound Bill",
+        description: "Obverse"
+    },
+    {
+        src: "../images/collection/scotland/clydesdale-bank/2016/5-pound-b.jpg",
+        alt: "Back of the 5 Pound Bill",
+        description: "Reverse"
+    }
+];
 createSlideshow("slide" + index, images[index]);
 new Slideshow("slideshow" + index);
 generateSlideShowInfo(
